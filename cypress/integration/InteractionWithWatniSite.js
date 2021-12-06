1a835c0460fd89f94f4e20d2a97fce5a02806999
fatma shawaf <fatma.naeem93@gmail.com>
2021-12-06 15:45:48 +0200

    edit foramt of file

--- cypress/integration/InteractionWithWatniSite.js
+++ cypress/integration/InteractionWithWatniSite.js
@@ -38,10 +38,10 @@ describe('add and delete monitor product', () => {
         context('Adding monitor with lowest price', () => {
             it('Verify hovering and clicking on add to cart button', () => {
                 cy.get('.products-row > :nth-child(1)').trigger('mouseover', 'center').find('.btn-add-cart').click({ force: true })
-                cy.get('.products-row > :nth-child(1) .product-name').then( (el)=> {
+                cy.get('.products-row > :nth-child(1) .product-name').then((el) => {
                     product_name = el.text()
                 })
-                cy.get(":nth-child(1) > .product-item > .product-price > .woocommerce-Price-amount > bdi").then((el)=>{
+                cy.get(":nth-child(1) > .product-item > .product-price > .woocommerce-Price-amount > bdi").then((el) => {
                     cy.wrap(el.text()).as('product_price1')
                 })
             });
@@ -58,10 +58,10 @@ describe('add and delete monitor product', () => {
                 cy.get('.mini_cart_control .counter').should('have.text', '1')
             });
             it('Verify the same item adding to shopping cart', () => {
-                cy.get(".mini-cart-items .product-name a").first().should("contain",product_name.trim().slice(1,10))
+                cy.get(".mini-cart-items .product-name a").first().should("contain", product_name.trim().slice(1, 10))
             });
-            it('Verify the same price ammount adding to shopping cart',function ()  {
-                cy.get('.product-amount > .woocommerce-Price-amount > bdi').should("contain",this.product_price1)
+            it('Verify the same price ammount adding to shopping cart', function () {
+                cy.get('.product-amount > .woocommerce-Price-amount > bdi').should("contain", this.product_price1)
             });
             // it('Verify subtotal amount', () => {
             //     cy.get(".sub-total-amount")
@@ -74,18 +74,18 @@ describe('add and delete monitor product', () => {
         context('Adding another monitor', () => {
             it('Verify choosing the lowest price menitor', () => {
                 cy.get('.products-row > :nth-child(2)').click()
-                cy.get('.products-row > :nth-child(1) .product-name').then( (el)=> {
+                cy.get('.products-row > :nth-child(1) .product-name').then((el) => {
                     cy.wrap(el.text()).as('product_name2')
                 })
-                cy.get(":nth-child(1) > .product-item > .product-price > .woocommerce-Price-amount > bdi").then((el)=>{
+                cy.get(":nth-child(1) > .product-item > .product-price > .woocommerce-Price-amount > bdi").then((el) => {
                     cy.wrap(el.text()).as('product_price2')
                 })
             });
-            it.skip('Verify the same item adding', function() {
-                cy.get(".product_title").first().should("contain",product_name2.trim().slice(1,10))
+            it.skip('Verify the same item adding', function () {
+                cy.get(".product_title").first().should("contain", product_name2.trim().slice(1, 10))
             });
-            it.skip('Verify the same price ammount adding',function ()  {
-                cy.get('.summary > .product-price > .woocommerce-Price-amount > bdi',{timeout:10000}).should("contain",this.product_price2)
+            it.skip('Verify the same price ammount adding', function () {
+                cy.get('.summary > .product-price > .woocommerce-Price-amount > bdi', { timeout: 10000 }).should("contain", this.product_price2)
             });
             it('Verify adding tow amount of product', () => {
                 cy.get(".quantity .jcf-btn-inc").click()
@@ -95,16 +95,16 @@ describe('add and delete monitor product', () => {
                 cy.get('.header-mini-cart').should("be.visible")
             });
             it('Verify the lenght of shopping cart item', () => {
-                cy.get('div.mini-cart-body .cart-item').should("have.length","2")
+                cy.get('div.mini-cart-body .cart-item').should("have.length", "2")
             });
             // it('Verify subtotal amount', () => {
             //     cy.get(".sub-total-amount")
             // });
             it('Verify adding the same amount to cart', () => {
-                cy.get(".cart-quantity input").should('have.value','1')
+                cy.get(".cart-quantity input").should('have.value', '1')
             });
             it('Verify adding the same amount shopping cart', () => {
-                cy.get('.mini_cart_control .counter').should('have.text','3')
+                cy.get('.mini_cart_control .counter').should('have.text', '3')
             });
         })
 
